13c67e7fb0dd5953f85290f03ffceabf408e5f98
yibsonalexis <yibsonleudo@Yibsons-MacBook-Pro.local>
2022-02-04 10:23:35 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { memo, useMemo, useState, useCallback } from "react";
+/** Try to avoid unnecessary re-renders changing the code below */
 
-function App() {
+const Child = memo(({ style, plusOne }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p style={style}>
+        child last render timestamp: {new Date().toISOString()}
+      </p>
+      <button onClick={() => plusOne((current) => current + 1)}>+1</button>
     </div>
   );
-}
+});
 
+const App = () => {
+  const [value, setValue] = React.useState("");
+  const [count, setCount] = useState(0);
+  const childStyles = useMemo(() => ({ backgroundColor: "yellow" }), []);
+
+  const plusOne = useCallback(() => {
+    setCount((current) => current + 1);
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>React - Hooks</h1>
+      <h2>Avoid unnecessary renderings!</h2>
+
+      <input value={value} onChange={(e) => setValue(e.target.value)} />
+      <p>input value is: "{value}"</p>
+      <p>Count: "{count}"</p>
+      <Child style={childStyles} plusOne={plusOne} />
+    </div>
+  );
+};
 export default App;
